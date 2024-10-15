857759c8b8bc12aaf9ed6c39f26d48cb1f6b9f0a
Hector Leiva <hector@hectorleiva.com>
2024-10-14 20:21:07 -0400

    zipcodes need to varchars since they can start with a leading zero

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE zipcode_polygons;
 \connect zipcode_polygons;
 
 CREATE TABLE zipcode_polygons_table (
-    zipcode NUMERIC,
+    zipcode varchar(5) PRIMARY KEY,
     geometric_polygons GEOMETRY
 );
 
